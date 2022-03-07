1aa6430c65d60a0b8b05c0683b507ead4025b188
Brian Yoon <infobyoon@gmail.com>
2022-03-07 06:44:30 -0800

    Commit 9: menu can change names after clicking different options

--- src/App.js
+++ src/App.js
@@ -13,9 +13,24 @@ function App() {
 		fastGas: '',
 	});
 	const [ethBalance, setEthBalance] = useState();
-	const [searchMenu, setSearchMenu] = useState({
-		menuEl: '',
-	});
+	const [searchMenu, setSearchMenu] = useState([
+		{
+			name: 'fvckrender',
+			wallet: '0xfded90a3b1348425577688866f798f94d77a0d02',
+		},
+		{
+			name: 'Zeneca_33',
+			wallet: '0x886478d3cf9581b624cb35b5446693fc8a58b787',
+		},
+		{
+			name: 'andy8052',
+			wallet: '0x90e5aa59a9df2add394df81521dbbed5f3c4a1a3',
+		},
+		{
+			name: 'DeezeFi',
+			wallet: '0xc46db2d89327d4c41eb81c43ed5e3dff111f9a8f',
+		},
+	]);
 	return (
 		<div>
 			<Header />
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,24 +2,24 @@ import React from 'react';
 import { useState } from 'react';
 
 function Menu({ searchMenu, setSearchMenu }) {
-	const collectors = [
-		{
-			name: 'fvckrender',
-			wallet: '0xfded90a3b1348425577688866f798f94d77a0d02',
-		},
-		{
-			name: 'Zeneca_33',
-			wallet: '0x886478d3cf9581b624cb35b5446693fc8a58b787',
-		},
-		{
-			name: 'andy8052',
-			wallet: '0x90e5aa59a9df2add394df81521dbbed5f3c4a1a3',
-		},
-		{
-			name: 'DeezeFi',
-			wallet: '0xc46db2d89327d4c41eb81c43ed5e3dff111f9a8f',
-		},
-	];
+	// const collectors = [
+	// 	{
+	// 		name: 'fvckrender',
+	// 		wallet: '0xfded90a3b1348425577688866f798f94d77a0d02',
+	// 	},
+	// 	{
+	// 		name: 'Zeneca_33',
+	// 		wallet: '0x886478d3cf9581b624cb35b5446693fc8a58b787',
+	// 	},
+	// 	{
+	// 		name: 'andy8052',
+	// 		wallet: '0x90e5aa59a9df2add394df81521dbbed5f3c4a1a3',
+	// 	},
+	// 	{
+	// 		name: 'DeezeFi',
+	// 		wallet: '0xc46db2d89327d4c41eb81c43ed5e3dff111f9a8f',
+	// 	},
+	// ];
 	const handleChange = ({ searchMenu, setSearchMenu }) => {
 		searchMenu.preventDefault();
 	};
@@ -31,7 +31,12 @@ function Menu({ searchMenu, setSearchMenu }) {
 				placeholder='Search here'
 				onChange={handleChange}
 				value={searchMenu.menuEl}>
-				{collectors.map((collector) => (
+				{/* {collectors.map((collector) => (
+					<option key={collector.value} value={collector.value}>
+						{collector.name}
+					</option>
+				))} */}
+				{searchMenu.map((collector) => (
 					<option key={collector.value} value={collector.value}>
 						{collector.name}
 					</option>
