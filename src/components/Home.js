d99507baad3cf8f2242f9994abfd090768d38aa0
Brian Yoon <infobyoon@gmail.com>
2022-03-08 10:21:35 -0800

    Commit 16: more grid formatting

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import CollectorDetails from './CollectorDetails';
 function Home({ searchMenu, setSearchMenu, handleSearch, ethBalance }) {
 	return (
 		<div>
-			<h1>This is the home page</h1>
+			<h1>How much Eth does this person have?</h1>
 			<Menu
 				searchMenu={searchMenu}
 				setSearchMenu={setSearchMenu}
